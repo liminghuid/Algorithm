9d144b59af2b293e885a2cc7986049c30bd0196f
liminghuid <171996135@qq.com>
2016-07-01 09:46:28 +0800

    添加dev.php

---
+++ dev.php
@@ -0,0 +1,22 @@
+<?php
+	//这里是dev分支里开发的内容
+	//int	
+$i = 1;
+$j = 2;
+//string	
+$a = "aaa";
+$b = "bbb";
+$c = "ccc";
+//array
+$arr1 = array(1,2,3,4);
+//class
+class T{
+	private $name;
+	private $email;
+	
+	public function out(){
+		echo $this->name;
+	}
+}
+	
+?>
